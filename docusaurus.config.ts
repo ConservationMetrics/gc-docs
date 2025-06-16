44b0329b73a7ba534571f590a197c6fcc5e36b55
Nico Pace <nicopace@gmail.com>
2025-06-16 16:12:40 -0300

    Sets up main content groups and removes example data.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Guardian Connector Documentation',
+  tagline: '',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://guardianconnector.net/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ConservationMetrics/gc-docs',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -58,20 +58,44 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Guardian Connector Documentation',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Guardian Connector Logo',
         src: 'img/logo.svg',
       },
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'referenceSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Reference',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          type: 'docSidebar',
+          sidebarId: 'integratedToolsSidebar',
+          position: 'left',
+          label: 'Integrated Tools',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'hostingSidebar',
+          position: 'left',
+          label: 'Hosting Options',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'howToSidebar',
+          position: 'left',
+          label: 'How-Tos',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialsSidebar',
+          position: 'left',
+          label: 'Tutorials',
+        },
+        {
+          href: 'https://github.com/ConservationMetrics/gc-docs',
           label: 'GitHub',
           position: 'right',
         },
@@ -79,44 +103,8 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Conservation Metrics, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,

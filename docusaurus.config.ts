00cabeaa8fb507c900f0567063fa05778a6744dc
Nico Pace <nicopace@gmail.com>
2025-06-17 13:59:00 -0300

    Configures repository for GitHub pages.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://guardianconnector.net/',
+  url: 'https://conservationmetrics.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gc-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ConservationMetrics', // Usually your GitHub org/user name.
+  projectName: 'gc-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

9f7ac413b138091dda1a319f40759b5ad3cba2bc
Nico Pace <nicopace@gmail.com>
2025-09-11 23:55:00 -0300

    Fixes issue with server side rendering.

--- src/components/useUrlParam.tsx
+++ src/components/useUrlParam.tsx
@@ -1,4 +1,6 @@
+import { useLocation } from '@docusaurus/router';
 export default function useUrlParam(paramName, defaultValue) {
+  const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const paramValue = searchParams.get(paramName) || defaultValue;
   return paramValue;
